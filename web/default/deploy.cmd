633b5890950f6c9370c07bd894e97b9d538b84e7
PopeyeZhong <9555843@qq.com>
2025-02-28 10:15:38 +0800

    更新部署命令脚本文件。 💳

--- web/default/deploy.cmd
+++ web/default/deploy.cmd
@@ -4,6 +4,12 @@ SET scheme=
 SET /p scheme=Please enter the scheme name you want to deploy: 
 if "%scheme%"=="" (SET scheme=default)
 
+SET environment=%Environment%
+SET /p environment=Please enter the environment name you want to deploy(%Environment%:[development^|test^|production]): 
+if "%environment%"=="" (
+	if "%Environment%"=="" (SET environment=development) else (SET environment=%Environment%)
+)
+
 SET debug=
 SET /p debug=Do you want to turn on remote debug mode?(On/Off)
 if "%debug%"=="" (SET debug=on)
@@ -15,17 +21,18 @@ SET value=
 SET /p value=Please enter the platform(windows/linux/mac) you want to deploy: 
 if "%value%" neq "" (SET platform=%value%)
 
-dotnet deploy                ^
-	-verbosity:quiet     ^
-	-overwrite:latest    ^
-	-host:web            ^
-	-site:default        ^
-	-scheme:%scheme%     ^
-	-debug:%debug%       ^
-	-edition:Debug       ^
-	-framework:net9.0    ^
-	-platform:%platform% ^
-	-architecture:x64    ^
-	.deploy              ^
+dotnet deploy                      ^
+	-verbosity:quiet           ^
+	-overwrite:latest          ^
+	-host:web                  ^
+	-site:default              ^
+	-scheme:%scheme%           ^
+	-environment:%environment% ^
+	-debug:%debug%             ^
+	-edition:Debug             ^
+	-framework:net9.0          ^
+	-platform:%platform%       ^
+	-architecture:x64          ^
+	.deploy                    ^
 	..\\..\\.deploy\\%scheme%\\^$^(host^).deploy ^
 	..\\..\\.deploy\\%scheme%\\^$^(site^).deploy
