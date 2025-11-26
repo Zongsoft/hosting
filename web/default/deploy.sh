3f1be500bf65ed613269a5e1e9fc99142c346cb3
PopeyeZhong <9555843@qq.com>
2025-11-26 17:26:36 +0800

    升级依赖库版本，并添加 OpenAPI 插件库的部署。 🐂

--- web/default/deploy.sh
+++ web/default/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dotnet deploy -host:web -site:default -edition:Debug -framework:net9.0 -architecture:x64
+dotnet deploy -host:web -site:default -edition:Debug -framework:net10.0 -architecture:x64
