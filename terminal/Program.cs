2868a56eec8054e748341f5bf68cd12357c396e4
PopeyeZhong <9555843@qq.com>
2026-03-27 17:32:29 +0800

    移除终端宿主程序对 Velopack 的依赖。 🍧

--- terminal/Program.cs
+++ terminal/Program.cs
@@ -8,8 +8,6 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		Velopack.VelopackApp.Build().Run();
-
 		Zongsoft.Plugins.Hosting.Application
 			.Terminal([.. args, "host=terminal", "site=daemon"])
 			.Run();
