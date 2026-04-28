1c252d18f6803af9a27ea759dcbaeb73af829707
PopeyeZhong <9555843@qq.com>
2026-04-28 20:48:06 +0800

    调整 Daemon 宿主程序支持 Windows 后台服务。 🌳

--- daemon/Program.cs
+++ daemon/Program.cs
@@ -8,16 +8,22 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		#if WINDOWS
-		Console.WriteLine("Windows");
-		#elif LINUX
-		Console.WriteLine("Linux");
-		#else
-		Console.WriteLine("Other");
-		#endif
-
+#if WINDOWS
 		Zongsoft.Plugins.Hosting.Application
-			.Daemon("zongsoft.daemon", [.. args, "host=daemon", "site=daemon", "daemon=zongsoft.daemon"])
+			.Daemon("zongsoft.daemon", [.. args, "host=daemon", "site=daemon"], builder =>
+			{
+				builder.Services.AddWindowsService(options => options.ServiceName = builder.Environment.ApplicationName);
+			}).Run();
+#elif LINUX
+		Zongsoft.Plugins.Hosting.Application
+			.Daemon("zongsoft.daemon", [.. args, "host=daemon", "site=daemon"], builder =>
+			{
+				builder.Services.AddSystemd();
+			}).Run();
+#else
+		Zongsoft.Plugins.Hosting.Application
+			.Daemon("zongsoft.daemon", [.. args, "host=daemon", "site=daemon"])
 			.Run();
+#endif
 	}
 }
