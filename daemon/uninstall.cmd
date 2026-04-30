6cd443d202ebbb8db18c1b687763f65e2c952496
PopeyeZhong <9555843@qq.com>
2026-04-30 16:50:13 +0800

    为卸载服务脚本添加状态查询和停止功能。 💥

--- daemon/uninstall.cmd
+++ daemon/uninstall.cmd
@@ -13,6 +13,19 @@ set /p SERVICE_NAME="Enter service name to uninstall(default: zongsoft.daemon): 
 if "%SERVICE_NAME%"=="" set SERVICE_NAME=zongsoft.daemon
 echo.
 
+sc query "%SERVICE_NAME%" >nul 2>nul
+if errorlevel 1 (
+	echo [ERROR] The '%SERVICE_NAME%' service does not exist.
+	exit /b 1
+)
+
+sc query "%SERVICE_NAME%" | find "RUNNING" >nul
+if !ERRORLEVEL! equ 0 (
+	echo The '%SERVICE_NAME%' service is running, stop it now...
+	sc stop "%SERVICE_NAME%" >nul
+)
+
+echo.
 sc delete "%SERVICE_NAME%"
 if !ERRORLEVEL! equ 0 (
 	echo The '%SERVICE_NAME%' service has been successfully deleted.
