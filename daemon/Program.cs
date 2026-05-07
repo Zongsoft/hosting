162a3c5eeaac0abba4dde28ebe0aec6e22070d1d
PopeyeZhong <9555843@qq.com>
2026-05-07 14:01:22 +0800

    调整 Daemon 宿主程序的主函数代码格式。 🔍

--- daemon/Program.cs
+++ daemon/Program.cs
@@ -8,22 +8,22 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-#if WINDOWS
+		#if WINDOWS
 		Zongsoft.Plugins.Hosting.Application
 			.Daemon("zongsoft.daemon", [.. args, "host=daemon", "site=daemon"], builder =>
 			{
 				builder.Services.AddWindowsService(options => options.ServiceName = builder.Environment.ApplicationName);
 			}).Run();
-#elif LINUX
+		#elif LINUX
 		Zongsoft.Plugins.Hosting.Application
 			.Daemon("zongsoft.daemon", [.. args, "host=daemon", "site=daemon"], builder =>
 			{
 				builder.Services.AddSystemd();
 			}).Run();
-#else
+		#else
 		Zongsoft.Plugins.Hosting.Application
 			.Daemon("zongsoft.daemon", [.. args, "host=daemon", "site=daemon"])
 			.Run();
-#endif
+		#endif
 	}
 }
